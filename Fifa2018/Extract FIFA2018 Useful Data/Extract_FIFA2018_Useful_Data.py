052539c8a28e3158237227d76d7069c92117bdbe
vkozyr <volodymyr_kozyr@sfu.ca>
2017-12-02 13:01:03 -0800

    Final Data for Neural Network Fifa 2018

--- Fifa2018/Extract FIFA2018 Useful Data/Extract_FIFA2018_Useful_Data.py
+++ Fifa2018/Extract FIFA2018 Useful Data/Extract_FIFA2018_Useful_Data.py
@@ -3,49 +3,20 @@ import pandas as pd
 fifa2018 = pd.read_csv('fifa2018.csv')   
 id = pd.read_csv('fifa_api_id.csv')
 useful_columns = ['ID', 
-                'age',
-                'height_cm',
-                'weight_kg',
-                'overall',
-                'pac',
-                'sho',
-                'pas',
-                'dri',
-                'def',
-                'phy',
-                'skill_moves',
-                'weak_foot',
-                'work_rate_att',
-                'work_rate_def',
-                'crossing',
-                'finishing',
-                'heading_accuracy',
-                'short_passing',
-                'volleys',
-                'dribbling',
-                'curve',
-                'free_kick_accuracy',
-                'long_passing',
-                'ball_control',
-                'acceleration',
-                'sprint_speed',
-                'agility',
-                'reactions',
-                'balance',
-                'shot_power',
-                'jumping',
-                'stamina',
-                'strength',
-                'long_shots',
-                'aggression',
-                'interceptions',
-                'positioning',
-                'vision',
-                'penalties',
-                'composure',
-                'marking',
-                'standing_tackle',
-                'sliding_tackle']
+                    'age', 
+                    'height_cm', 
+                    'weight_kg', 
+                    'overall', 
+                    'pac', 
+                    'sho', 
+                    'pas', 
+                    'dri',
+                    'def', 
+                    'phy', 
+                    'skill_moves', 
+                    'weak_foot', 
+                    'work_rate_att', 
+                    'work_rate_def']
 
 fifa2018 = pd.read_csv('fifa2018.csv')                         
 
@@ -54,4 +25,4 @@ filtered_players = players[players['ID'].isin(id['fifa_api_id'])]
 
 print(filtered_players)
 
-filtered_players.to_csv('f18_players.csv')
+players.to_csv('filtered_fifa2018_players.csv')
