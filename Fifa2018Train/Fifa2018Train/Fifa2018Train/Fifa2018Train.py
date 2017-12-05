d31332f8cc0ddb6d7753354c594f01377c0fbde3
vkozyr <volodymyr_kozyr@sfu.ca>
2017-12-05 09:02:04 -0800

    added output files

--- Fifa2018Train/Fifa2018Train/Fifa2018Train/Fifa2018Train.py
+++ Fifa2018Train/Fifa2018Train/Fifa2018Train/Fifa2018Train.py
@@ -70,6 +70,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense
 data2018 = pd.read_csv('shuffled_filtered_fifa2018_players.csv')
+ID = data2018['ID']
 del data2018['ID']
 
 train_data2018 = data2018.ix[:458,:]
@@ -95,12 +96,13 @@ Y_test2018 = test_overall_column2018.as_matrix()
 
 model2018 = Sequential()
 
-model2018.add(Dense(units=200, activation='relu', input_shape=(17,),use_bias=True, kernel_initializer='random_uniform',bias_initializer='random_uniform'))
+model2018.add(Dense(units=20, activation='relu', input_shape=(17,),use_bias=True, kernel_initializer='random_uniform',bias_initializer='random_uniform'))
+model2018.add(Dense(units=20, activation='relu', use_bias=True, kernel_initializer='random_uniform',bias_initializer='random_uniform'))
 model2018.add(Dense(units=1, activation='linear', use_bias=True,kernel_initializer='random_uniform', bias_initializer='random_uniform'))
 
 
 model2018.compile(loss='mean_absolute_error',optimizer=keras.optimizers.SGD(lr=0.031,momentum=0.1))
-model2018.fit(x=X_train2018, y=Y_train2018, batch_size=458, epochs=500)
+model2018.fit(x=X_train2018, y=Y_train2018, batch_size=458, epochs=1000)
 
 print('MODEL 2018')
 print(model2018.evaluate(x=X_test2018, y=Y_test2018))
@@ -138,42 +140,52 @@ Y_test_features = test_overall_column_features.as_matrix()
 
 model_features = Sequential()
 
-model_features.add(Dense(units=1000, activation='relu', input_shape=(89,), use_bias=True, kernel_initializer='random_uniform', bias_initializer='random_uniform'))
+model_features.add(Dense(units=50, activation='relu', input_shape=(89,), use_bias=True, kernel_initializer='random_uniform', bias_initializer='random_uniform'))
+model_features.add(Dense(units=50, activation='relu', use_bias=True, kernel_initializer='random_uniform',bias_initializer='random_uniform'))
 model_features.add(Dense(units=6, activation='linear', use_bias=True, kernel_initializer='random_uniform', bias_initializer='random_uniform'))
 
 
 model_features.compile(loss='mean_absolute_error', optimizer=keras.optimizers.SGD(lr=0.031,momentum=0.3))
-model_features.fit(x=X_train_features, y=Y_train_features, batch_size=400, epochs=2000)
+model_features.fit(x=X_train_features, y=Y_train_features, batch_size=400, epochs=1000)
 
 print('MODEL 2016-2017')
 print(model_features.evaluate(x=X_test_features, y=Y_test_features))
-predicted_features=model_features.predict(X_train_features)
-print(predicted_features[0][0])
+predicted_features = model_features.predict(X_train_features)
 
-t2018=model2018.predict(X_train2018)
-print (t2018)
 
-np.savetxt('test.txt',X_train2018,fmt='%.2f')
+t2018 = model2018.predict(X_train2018)
+#print(t2018)
+
+
+
+
 
 for i in range(len(train_data_with_added_columns2018)):
     for j in range(3,9):
-        X_train2018[i][j]=predicted_features[i][j-3]/120
+        X_train2018[i][j] = predicted_features[i][j - 3] / 120
+        #ID[i].ix[j] = X_train2018[i][j]
+#print(ID)
+#import csv
 
-np.savetxt('predicted.txt',X_train2018,fmt='%.2f')
+#with open("output.csv", "w") as f:
+#    writer = csv.writer(f)
+#    writer.writerows(predicted_features)
 
-print ('FINALYYY')
+#print(X_train2018)
 
-pr2018=model2018.predict(X_train2018)
-print (pr2018)
+print('FINALYYY')
 
-abserr=0
+pr2018 = model2018.predict(X_train2018)
+#print(pr2018)
+
+abserr = 0
 for x in range(len(t2018)):
-    a=abs(t2018[x]-pr2018[x])
-    print (a)
+    a = abs(t2018[x] - pr2018[x])
+    print(a)
     abserr+=a
 abserr/=len(t2018)
-print ('errrorr')
-print (abserr)
+print('errrorr')
+print(abserr)
 
 
 #print(model2018.evaluate(x=X_test2018,y=Y_test2018))
